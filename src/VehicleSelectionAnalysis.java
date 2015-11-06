62816871c4aac02cc6595c97a63296ebd1e1798e
sdouglass24 <sdouglass24@hotmail.com>
2015-11-06 14:57:39 -0500

    started VehicleSelection class

--- src/VehicleSelectionAnalysis.java
+++ src/VehicleSelectionAnalysis.java
@@ -1,4 +1,27 @@
-
-public class VehicleSelectionAnalysis {
+public class VehicleSelectionAnalysis 
+{
+	Vehicle selectedVehicle;
 	
+
+
+	public VehicleSelectionAnalysis()
+	{
+	
+	}
+	
+	public Vehicle getClosestVehicle(Location location)
+	{
+		
+	}
+	
+	public void setSelectedVehicle(Vehicle vehicle)
+	{
+		selectedVehicle = vehicle;
+	}
+	
+	public Vehicle getSelectedVehicle()
+	{
+		return selectedVehicle;
+	}
+
 }
