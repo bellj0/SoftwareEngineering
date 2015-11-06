18ab7ae1ffadca18ab5096116a82f024a0c72006
sdouglass24 <sdouglass24@hotmail.com>
2015-11-06 11:13:16 -0500

    Vehicle class

---
+++ PoliceCommandControl/src/Vehicle.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+
+public class Vehicle 
+{
+	private String vehicleType;
+	private int location;
+	private boolean available;
+	private ArrayList<Vehicle> availableVehicles = new ArrayList<Vehicle>();
+	private ArrayList<Vehicle> allVehicles = new ArrayList<Vehicle>();
+
+
+	public Vehicle(String type, int location)
+	{
+		setVehicleType(type);
+		setLocation(location);	
+		allVehicles.add(this);
+	}
+
+	public void setVehicleType(String type)
+	{
+		if (type.equals(null) || type.equals(""))
+		{
+			throw new NullPointerException("Vehicle type cannot be a null or empty string.");
+			//to-do : redirect them to re-enter it / or choose car based on best information available
+		}
+		else
+		{
+			this.vehicleType = type;
+		}
+	}
+	
+	public String getVehicleType()
+	{
+		return this.vehicleType;
+	}
+	
+	public void setLocation(int location)
+	{
+		this.location = location;
+	}
+	
+	public int getLocation()
+	{
+		return this.location;
+	}
+	
+	public ArrayList<Vehicle> getAvailableVehicles()
+	{
+		for (Vehicle v: allVehicles){
+			if (v.isAvailable() == true)
+			{
+				availableVehicles.add(v);
+			}
+		}
+		
+		return availableVehicles;
+	}
+	
+	public void setAvailable(boolean available){
+		this.available = available;
+	}
+	
+	public boolean isAvailable()
+	{
+		return this.available;
+	}
+
+}
---
+++ PoliceCommandControl/src/VehicleSelectionAnalysis.java
@@ -0,0 +1,4 @@
+
+public class VehicleSelectionAnalysis {
+	// testing git hub
+}
