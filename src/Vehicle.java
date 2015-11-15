f308b005c161f8b65b696e5a1330abdc71333ee4
sdouglass24 <sdouglass24@hotmail.com>
2015-11-15 00:38:00 -0500

    Added documentation/comments to the Vehicle class

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,3 +1,6 @@
+/**
+ * This class represents the vehicles in the Rowan PD force.  
+ */
 import java.util.ArrayList;
 
 public class Vehicle 
@@ -8,7 +11,11 @@ public class Vehicle
 	private ArrayList<Vehicle> availableVehicles = new ArrayList<Vehicle>();
 	private ArrayList<Vehicle> allVehicles = new ArrayList<Vehicle>();
 
-
+/**
+ * Creates a new vehicle object and stores it in the list of all vehicles.
+ * @param type The vehicle's type (cruiser, K9, etc).
+ * @param location The current location of the vehicle.
+ */
 	public Vehicle(String type, Location location)
 	{
 		setVehicleType(type);
@@ -16,6 +23,10 @@ public class Vehicle
 		allVehicles.add(this);
 	}
 
+/**
+ * Sets the vehicle's type. 
+ * @param type The type of the current vehicle.
+ */
 	public void setVehicleType(String type)
 	{
 		if (type.equals(null) || type.equals(""))
@@ -28,25 +39,42 @@ public class Vehicle
 			this.vehicleType = type;
 		}
 	}
-	
+
+/**
+ * Returns the vehicle's type.
+ * @return String that holds the vehicle's type.
+ */
 	public String getVehicleType()
 	{
 		return this.vehicleType;
 	}
-	
+
+/**
+ * Sets the vehicle's current location.
+ * @param location The current location to be set.
+ */
 	public void setLocation(Location location)
 	{
 		this.location = location;
 	}
-	
+
+/**
+ * Returns the vehicle's current location. 
+ * @return Location object representing the vehicle's current location.
+ */
 	public Location getLocation()
 	{
 		return this.location;
 	}
-	
+
+/**
+ * Returns the list of all vehicles that are available to respond to an incident. 
+ * @return ArrayList containing all available vehicles.
+ */
 	public ArrayList<Vehicle> getAvailableVehicles()
 	{
-		for (Vehicle v: allVehicles){
+		for (Vehicle v: allVehicles)
+		{
 			if (v.isAvailable() == true)
 			{
 				availableVehicles.add(v);
@@ -55,11 +83,20 @@ public class Vehicle
 		
 		return availableVehicles;
 	}
-	
-	public void setAvailable(boolean available){
+
+/**
+ * Sets a vehicle's availability status to either true or false based on the value of the boolean parameter supplied.
+ * @param available Boolean value representing whether the vehicle is available (true) or unavailable (false).
+ */
+	public void setAvailablity(boolean available)
+	{
 		this.available = available;
 	}
-	
+
+/**
+ * Returns a vehicle's availability status. 
+ * @return The availability status of the vehicle, where true is available and false is unavailable. 
+ */
 	public boolean isAvailable()
 	{
 		return this.available;
--- src/VehicleSelectionAnalysis.java
+++ src/VehicleSelectionAnalysis.java
@@ -12,8 +12,11 @@ public class VehicleSelectionAnalysis
 	public Vehicle getClosestVehicle(Location location)
 	{
 		Vehicle closest = null;
-		
-		
+			for (Vehicle v: closest.getAvailableVehicles())
+			{
+			  // need compare method to compare the location of the incident to the vehicle's location
+			}
+				
 		return closest;
 	}
 	
