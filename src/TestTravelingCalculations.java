0dc3754ef7563d33ee52cbea9d7657a364fe43d3
stumpr73 <ryanjeffs@comcast.net>
2015-11-28 16:20:24 -0500

    Added TestTravelingCalculations class

---
+++ src/TestTravelingCalculations.java
@@ -0,0 +1,45 @@
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+
+public class TestTravelingCalculations
+{
+	private Vehicle vehicle;
+	private Location currentLocation = Location.LIBRARY;
+	private Location newLocation = Location.ROBINSON;
+	private TravelingCalculations calculation = new TravelingCalculations(vehicle,newLocation);
+	
+	@Before
+	public void setUp() throws Exception 
+	{
+		vehicle = new Vehicle("Cruiser",currentLocation);
+	}
+
+	@After
+	public void tearDown() throws Exception 
+	{
+	
+	}
+	
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+
+	@Test
+	public void testCalculateDistance() 
+	{
+		assertEquals(120,calculation.calculateDistance());
+	}
+	
+	@Test
+	public void testConvertDistanceToTime() 
+	{
+		assertEquals(12000,calculation.convertDistanceToTime());
+	}
+}
