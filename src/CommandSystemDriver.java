222ee3a3d1b4ba378f09a647868d37e860446555
sdouglass24 <sdouglass24@hotmail.com>
2015-12-03 15:17:43 -0500

    Added a new driver class CommandSystemDriver

---
+++ src/CommandSystemDriver.java
@@ -0,0 +1,35 @@
+import java.awt.EventQueue;
+
+/**
+ * This class contains the main method and instantiates the system with the necessary vehicle and incident information.
+ * 
+ *
+ */
+
+public class CommandSystemDriver 
+{
+
+	public static void main(String[] args) 
+	
+	// Create and display the application's GUI. 
+	{
+		EventQueue.invokeLater(new Runnable() 
+		{
+			public void run() 
+			{
+				try 
+				{
+					VehicleCommandGUI frame = new VehicleCommandGUI();
+					frame.setVisible(true);
+					frame.pack();
+				} 
+				catch (Exception e) 
+				{
+					e.printStackTrace();
+				}
+		   }
+		});
+
+	}
+
+}
